20d956141a2f4091690440adad9e643472d7313e
mediote <mediote90@gmail.com>
2024-02-24 14:34:49 -0300

    Improve setup.py and pyproject.toml

--- setup.py
+++ setup.py
@@ -1,38 +1,37 @@
 from setuptools import find_packages, setup
+from setuptools_scm import get_version
 
-VERSION = '0.0.1'
-DESCRIPTION = 'Meu primeiro pacote em Python'
-LONG_DESCRIPTION = 'Meu primeiro pacote em Python com uma descrição um pouco mais longa'
-LONG_DESCRIPTION_CONTENT_TYPE = 'text/markdown'  # Altere conforme necessário
-
-requires = [
-    'boto3',
-    'pandas',
-    'requests',
-]
+DESCRIPTION = 'Pacotes MLOPS reutilizáveis.'
+LONG_DESCRIPTION = 'Pacotes MLOPS reutilizáveis.'
+LONG_DESCRIPTION_CONTENT_TYPE = 'text/markdown'
 
 setup(
     name="mlopspackages",
-    version=VERSION,
+    version=get_version(),
     author="Andre Mediote de Sousa",
     author_email="mediote90@gmail.com",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
     packages=find_packages(exclude=("tests*", "docs")),
-    install_requires=requires,
-    keywords=['python', 'first package'],
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'boto3',
+        'pandas',
+        'requests',
+    ],
+    keywords=['python', 'mlops', 'reusable packages'],
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",  # Modificado de Education para Developers
-        "License :: OSI Approved :: MIT License",  # Exemplo, altere conforme sua licença
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",  # Se for verdade, é uma prática melhor do que especificar sistemas operacionais
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',  # Especifique a versão mínima do Python
-    project_urls={  # URLs opcionais
-        "Bug Tracker": "https://github.com/seuusuario/mlopspackages/issues",
-        "Documentation": "https://github.com/seuusuario/mlopspackages#readme",
-        "Source Code": "https://github.com/seuusuario/mlopspackages",
+    python_requires='>=3.6',
+    project_urls={
+        "Bug Tracker": "https://github.com/mediote/mlopspackages/issues",
+        "Documentation": "https://github.com/mediote/mlopspackages#readme",
+        "Source Code": "https://github.com/mediote/mlopspackages",
     },
 )
